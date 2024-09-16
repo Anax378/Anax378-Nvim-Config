0f809d555e3b63bdc34e0197c9f7090e18c4a6c6
Anax378 <frantisek.mrazek06@seznam.cz>
2024-09-16 14:24:25 +0200

    Initial Commit

---
+++ lua/anax/init.lua
@@ -0,0 +1,161 @@
+
+vim.opt.number = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+print(563)
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+
+local lazy = {}
+function lazy.install(path)
+  if not vim.loop.fs_stat(path) then
+    print('Installing lazy.nvim....')
+    vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      path,
+    })
+  end
+end
+
+function lazy.setup(plugins)
+  if vim.g.plugins_ready then
+    return
+  end
+
+  -- You can "comment out" the line below after lazy.nvim is installed
+  lazy.install(lazy.path)
+
+  vim.opt.rtp:prepend(lazy.path)
+
+  require('lazy').setup(plugins, lazy.opts)
+  vim.g.plugins_ready = true
+end
+
+
+lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+lazy.opts = {}
+
+lazy.setup({
+	{'folke/tokyonight.nvim'},
+
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'L3MON4D3/LuaSnip'},
+
+	{'hrsh7th/nvim-cmp'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/cmp-buffer'},
+	{'hrsh7th/cmp-path'},
+	{'hrsh7th/cmp-cmdline'},
+	{'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = {'nvim-lua/plenary.nvim'}},
+	{'nvim-treesitter/nvim-treesitter'},
+	{'akinsho/toggleterm.nvim', version = "*", config = true},
+	{'lambdalisue/fern.vim'},
+	{ "savq/melange-nvim" },
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}},
+	{ "m00qek/baleia.nvim", tag = 'v1.4.0' },
+})
+
+require("ibl").setup({
+		indent = {char = "|"},
+		whitespace = { highlight = { "Whitespace", "NonText" }},
+});
+require("toggleterm").setup{};
+
+local lsp_zero = require("lsp-zero")
+
+lsp_zero.on_attach(function(client, bufnr)
+	lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = {"pyright", "jdtls"},
+	handlers = {
+		lsp_zero.default_setup,
+	},
+})
+
+
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+	{name = 'buffer'},
+  },
+  mapping = {
+    ['<C-y>'] = cmp.mapping.confirm({select = false}),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+	['<Tab>'] = cmp.mapping.confirm({select = false}),
+    ['<C-p>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+    ['<C-n>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_next_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+  },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+
+  preselect = 'item',
+  completion = {
+	  completeopt = 'menu,menuone,noinsert'
+  },
+
+})
+
+
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('melange')
+vim.api.nvim_exec('language en_US', true)
+
+function setExpandtab()
+	if vim.bo.filetype == "haskell" or vim.bo.filetype == "cabal" then
+		vim.o.expandtab = true
+	else
+		vim.o.expandtab = false
+	end
+end
+
+
+vim.cmd(
+[[
+set list
+set listchars=tab:>-
+augroup SetExpandTab
+	autocmd!
+	autocmd FileType * lua setExpandtab()
+augroup END
+
+]])
+
+require("anax.remap")
+
+
+
+
+
+
+
---
+++ lua/anax/remap.lua
@@ -0,0 +1,73 @@
+
+local builtin = require("telescope.builtin")
+
+local fontSize = 15;
+local font = [["JetBrains Mono"]]
+
+vim.cmd("set guifont=" .. font .. ":h" .. fontSize);
+
+function changeFontSize(amount)
+	fontSize = fontSize + amount;
+	vim.cmd("silent set guifont=" .. font .. ":h" .. fontSize);
+end
+
+function fern_buffer_init()
+	vim.cmd(
+	[[
+		nmap <buffer><expr>
+			  \ <Plug>(fern-my-expand-or-collapse-or-enter)
+			  \ fern#smart#leaf(
+			  \   "\<Plug>(fern-action-open)",
+			  \   "\<Plug>(fern-action-expand)",
+			  \   "\<Plug>(fern-action-collapse)",
+			  \ )
+
+		nmap <buffer><nowait> <space> <Plug>(fern-my-expand-or-collapse-or-enter)
+		nmap <buffer><nowait> <CR> <Plug>(fern-my-expand-or-collapse-or-enter)
+		nmap <buffer><nowait> C <Plug>(fern-action-enter)
+
+
+
+	]])
+end
+
+vim.cmd(
+[[
+	augroup fern_customizations
+		autocmd!
+		autocmd FileType fern lua fern_buffer_init()
+	augroup END
+]]
+)
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-w>", ":bd!<CR>", {silent=true})
+vim.keymap.set("n", "<C-n>", ":enew<CR>", {silent=true})
+vim.keymap.set("n", "<C-Tab>", ":bnext<CR>", {silent=true})
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+vim.keymap.set("n", "<A-Right>", "<C-w>w", {noremap = true, silent = true})
+vim.keymap.set("n", "<A-Left>", "<C-w>W", {noremap = true, silent = true})
+
+vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>", {silent = true})
+vim.keymap.set("t", "<C-e>", "<C-\\><C-n>", {noremap = true, silent = true})
+vim.keymap.set("n", "<F5>", ":split<CR>:terminal<CR>arun.bat<CR><C-\\><C-n>", {silent = true})
+
+vim.keymap.set("n", "tf", ":Fern . -drawer<CR>", {silent = true})
+
+vim.keymap.set("n", "<A-CR>", vim.diagnostic.open_float, {silent=true})
+
+vim.cmd([[command! Settings edit $MYVIMRC]])
+
+vim.cmd(
+[[
+let s:baleia = luaeval("require('baleia').setup({})")
+command! ANSI call s:baleia.once(bufnr('%'))
+]]
+)
+
+vim.keymap.set("n", "<C-,>", function() changeFontSize(1) end, {silent = true})
+vim.keymap.set("n", "<C-.>", function() changeFontSize(-1) end, {silent = true})
+
